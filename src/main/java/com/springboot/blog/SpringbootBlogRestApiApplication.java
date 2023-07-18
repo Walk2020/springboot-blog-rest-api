761d68f215530507c64ba026765bbd3271f0c675
Yang <zhyants@gmail.com>
2023-07-18 10:44:41 -0700

    update
    update AWS

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -32,7 +36,7 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/Walk2020/springboot-blog-rest-api"
 		)
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
@@ -42,4 +46,21 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+
+	// the following lines will be executed first when the application starts
+	// to insert some data into database
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
